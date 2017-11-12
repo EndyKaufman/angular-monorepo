942eba5eaeaa63e7d990e9dd4a4d54fd5ee9c540
endy <admin@site15.ru>
2017-11-13 03:40:47 +0500

    Remove ignored files

--- lib/dist/app/app.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { Injector } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-export declare class BaseAppService {
-    translateService: TranslateService;
-    constructor(injector: Injector);
-    translatedText(): any;
-}
